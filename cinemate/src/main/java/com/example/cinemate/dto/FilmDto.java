8ccb9f75d2b866b3964571b4ed79d47a67685798
BintangKantata <52192720+BintangKantata@users.noreply.github.com>
2025-12-07 23:22:47 +0700

    Update FilmDto.java

--- cinemate/src/main/java/com/example/cinemate/dto/FilmDto.java
+++ cinemate/src/main/java/com/example/cinemate/dto/FilmDto.java
@@ -1,6 +1,7 @@
 package com.example.cinemate.dto;
 
 import com.example.cinemate.entities.Genre;
+import com.example.cinemate.entities.RatingUsia;
 import lombok.Data;
 
 import java.util.Set;
@@ -10,7 +11,8 @@ public class FilmDto {
     private Long id;
     private String title;
     private Set<Genre> genres;
+    private RatingUsia ru;
     private int duration;
-    private int tahun;
+    private String image;
     private String description;
 }
