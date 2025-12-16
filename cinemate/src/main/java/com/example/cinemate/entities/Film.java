33df957eee303b4dd4bfc59973fd2877cd8500e5
BintangKantata <52192720+BintangKantata@users.noreply.github.com>
2025-12-16 13:08:47 +0700

    Update Film.java

--- cinemate/src/main/java/com/example/cinemate/entities/Film.java
+++ cinemate/src/main/java/com/example/cinemate/entities/Film.java
@@ -1,9 +1,12 @@
 package com.example.cinemate.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
+@Getter @Setter
 @Entity
 @Table(name = "film")
 public class Film {
@@ -28,26 +31,5 @@ public class Film {
     private String image;
 
     @Column(columnDefinition = "TEXT")
-    private String description; // deskripsi film
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public Set<Genre> getGenres() { return genres; }
-    public void setGenres(Set<Genre> genres) { this.genres = genres; }
-
-    public RatingUsia getRu(){ return ru;}
-    public void setRu(RatingUsia ru){ this.ru = ru; }
-
-    public int getDuration() { return duration; }
-    public void setDuration(int duration) { this.duration = duration; }
-
-    public String getImage() { return image; }
-    public void setImage(String image) { this.image = image; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    private String description; // synopsis
 }
