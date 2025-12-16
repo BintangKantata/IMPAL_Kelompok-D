648ad55b883bbdf86f588be1a2a98a4b745c5d40
BintangKantata <52192720+BintangKantata@users.noreply.github.com>
2025-12-16 13:03:20 +0700

    Update CustomerDto.java

--- cinemate/src/main/java/com/example/cinemate/dto/CustomerDto.java
+++ cinemate/src/main/java/com/example/cinemate/dto/CustomerDto.java
@@ -2,11 +2,14 @@ package com.example.cinemate.dto;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "customers", uniqueConstraints = {
         @UniqueConstraint(columnNames = "email")
 })
+@Getter @Setter
 public class CustomerDto {
 
     @Id
@@ -20,20 +23,4 @@ public class CustomerDto {
     private String email;
 
     private String password;
-
-    // Getter & Setter
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 }
